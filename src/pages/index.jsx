f030a2e99dbfc72f86693e019ec5a8b75b211656
Brian Nathan <comonlegend123@gmail.com>
2023-03-08 07:45:21 +0700

    resolve home

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,41 +4,7 @@ function Home() {
   const router = useRouter();
   return (
     <>
-      <TopBar />
-
-      <main className="w-8/12 m-auto">
-        <div className="m-5">
-          <Breadcrumb />
-        </div>
-
-        <div className="my-2 flex gap-4">
-          <button className="btn" onClick={getAuctionsLazy}>
-            Get Auction Lazy
-          </button>
-        </div>
-
-        <div className="mt-2 flex gap-6">
-          {/* <ItemCard
-            category="HELLO"
-            creatorName="HELLO"
-            title="HELLO"
-            timeStart="HELLO"
-            openPrice="HELLO"
-          /> */}
-          {auctionList.map((auction, index) => {
-            return (
-              <ItemCard
-                key={index}
-                category={auction.category}
-                creatorName={auction.creatorName}
-                title={auction.title}
-                timeStart={auction.timeStart}
-                openPrice={auction.openPrice}
-              />
-            );
-          })}
-        </div>
-      </main>
+      <main className="w-8/12 m-auto">Home Sweet Home</main>
     </>
   );
 }
